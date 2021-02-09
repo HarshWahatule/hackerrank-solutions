a3aceb9b29fb0e2f314a589811468bf548c88f77
harsing wahatule <wahatuleharsing@gmail.com>
2021-02-09 23:37:13 +0530

    changes of CompareTheTriplets

--- python/CompareTheTriplets.py
+++ python/CompareTheTriplets.py
@@ -8,7 +8,8 @@ import sys
 
 # Complete the compareTriplets function below.
 def compareTriplets(a, b):
-    Alice, Bob = (0, 0)
+    Alice = 0
+    Bob = 0
     for i in range(len(a)):
         if a[i] > b[i]:
             Alice += 1
